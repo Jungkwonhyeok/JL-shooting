2c7c598767bb43d892387b08f352b7fe414a6bdd
Jungkwonhyeok <786poqqep@gmail.com>
2025-08-30 21:52:12 +0900

    게임 기초

---
+++ Assets/Code/Background.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float speed;
+    public int startIndex;
+    public int endIndex;
+    public Transform[] sprites;
+
+    float viewHeight;
+
+    void Awake()
+    {
+        viewHeight = Camera.main.orthographicSize * 2;
+    }
+
+
+    void Update()
+    {
+        Move();
+        Scrlling();
+    }
+
+     void Move()
+    {
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = Vector3.down * speed * Time.deltaTime;
+        transform.position = curPos + nextPos;
+    }
+
+    void Scrlling()
+    {
+        if (sprites[endIndex].position.y < -viewHeight)
+        {
+            //스프리트 재사용
+            Vector3 backSpritePos = sprites[startIndex].localPosition;
+            Vector3 frontSpritePos = sprites[endIndex].localPosition;
+            sprites[endIndex].transform.localPosition = backSpritePos + Vector3.up * viewHeight;
+
+            //Index 바꾸기
+            int startIndexSave = startIndex;
+            startIndex = endIndex;
+            endIndex = (startIndexSave - 1 == -1) ? sprites.Length - 1 : startIndexSave - 1;
+        }
+    }
+}
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public bool isTouchTop;
+    public bool isTouchLeft;
+    public bool isTouchRight;
+    public bool isTouchBottom;
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        if ((isTouchRight && h == 1) || (isTouchLeft && h == -1))
+            h = 0;
+        float v = Input.GetAxisRaw("Vertical");
+        if ((isTouchTop && v == 1) || (isTouchBottom && v == -1))
+            v = 0;
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
+
+        transform.position = curPos + nextPos;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = true;
+                    break;
+                case "Left":
+                    isTouchLeft = true;
+                    break;
+                case "Right":
+                    isTouchRight = true;
+                    break;
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+                case "Right":
+                    isTouchRight = false;
+                    break;
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+            }
+        }
+    }
+}
