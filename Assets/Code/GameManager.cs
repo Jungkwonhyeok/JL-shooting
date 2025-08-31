bca13128471efe231a46d17c984016336437657b
Jungkwonhyeok <786poqqep@gmail.com>
2025-09-01 02:23:49 +0900

    적 완성

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] enemyObjs;
+    public Transform[] spawnPoint;
+
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
+    void Update()
+    {
+        curSpawnDelay += Time.deltaTime;
+
+        if(curSpawnDelay > maxSpawnDelay)
+        {
+            SpawnEnemy();
+            maxSpawnDelay = Random.Range(0.5f, 3f);
+            curSpawnDelay = 0;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        int ranEnemy = Random.Range(0, 3);
+        int ranPoint = Random.Range(0, 5);
+        Instantiate(enemyObjs[ranEnemy],
+            spawnPoint[ranPoint].position,
+            spawnPoint[ranPoint].rotation);
+    }
+}
