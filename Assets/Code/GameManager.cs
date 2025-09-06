0232d45c303412a6f9467da2ffe23dc875aa1769
Jungkwonhyeok <786poqqep@gmail.com>
2025-09-07 02:07:19 +0900

    보스 완성, 터지는거 만드는중

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -21,11 +21,9 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         spawnList = new List<Spawn>();
-<<<<<<< Updated upstream
+
         enemyObjs = new string[] {"EnemyS", "EnemyM" , "EnemyL","EnemyB" };
-=======
-        enemyObjs = new string[] {"EnemyS", "EnemyM" , "EnemyL" };
->>>>>>> Stashed changes
+
         ReadSpawnFile();
     }
 
@@ -89,12 +87,11 @@ public class GameManager : MonoBehaviour
             case "L":
                 enemyIndex = 2;
                 break;
-<<<<<<< Updated upstream
+
             case "B":
                 enemyIndex = 3;
                 break;
-=======
->>>>>>> Stashed changes
+
         }
         int enemyPoint = spawnList[spawnIndex].point;
         GameObject enemy = objectManager.MakeObj(enemyObjs[enemyIndex]);
